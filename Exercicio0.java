85ed8258b9e399550eff211d634e8ce22ec0e4d6
Matheus Henrique <mhenrique182005@gmail.com>
2026-03-10 23:40:29 -0300

    Add files via upload

---
+++ Exercicio0.java
@@ -0,0 +1,16 @@
+
+
+package com.mycompany.exercicio0;
+import classe.Telefone;
+
+public class Exercicio0 {
+
+    public static void main(String[] args) {
+        Telefone t1;
+        
+        t1=new Telefone();
+        t1.preencher();
+        System.out.println("");
+        t1.imprimir();
+    }
+}
