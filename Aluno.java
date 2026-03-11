1211ee8560c23baf673dfbf7b0b22d99b5a752f8
Matheus Henrique <mhenrique182005@gmail.com>
2026-03-10 23:44:07 -0300

    Add files via upload

---
+++ Aluno.java
@@ -0,0 +1,119 @@
+/*nome:String - idade:int - matricula:String - anoIngresso:int*/
+package com.mycompany.exercicio3;
+import java.util.Scanner;
+
+public class Aluno {
+    private String nome;
+    private int idade;
+    private int matricula;
+    private int anoIngresso;
+
+    public Aluno() {
+    }
+
+    public Aluno ( String nome, int idade, int matricula, int anoIngresso){
+        this.nome=nome;
+        this.idade=idade;
+        this.matricula=matricula;
+        this.anoIngresso= anoIngresso;
+    }
+    
+    public void copiar( Aluno al){
+        this.nome= al.nome;
+        this.idade=al.idade;
+        this.matricula=al.matricula;
+        this.anoIngresso=al.anoIngresso;
+        
+    }
+    
+    public void Preencher (){
+        Scanner ler =new Scanner(System.in);
+        System.out.println( "    PREENCHENDO DADOS    ");
+        System.out.println();
+        
+        System.out.println(" Digite seu nome:");
+        this.nome=ler.nextLine();
+        System.out.println ( " Insira sua idade:");
+        this.idade=ler.nextInt();
+        System.out.println(" Digite sua matricula:");
+        this.matricula = ler.nextInt();
+        System.out.println ( " Digite seu ano de ingressao:");
+        this.anoIngresso=ler.nextInt();
+        
+    }
+    
+    public void imprimir(){
+        System.out.println( "        DADOS        ");
+        
+        System.out.println( " Nome:" + nome);
+        System.out.println(" Idade:"+ idade);
+        System.out.println( " Matricula:" + matricula);
+        System.out.println( " Ano de Ingressao:" + anoIngresso);
+    }
+
+    public String getNome() {
+        return nome;
+    }
+    public int getIdade(){
+        return idade;
+    }
+    public int getMatricula(){
+        return matricula;
+    }
+        
+    public int getAnoIngresso (){
+        return anoIngresso;
+    }
+    
+    public void setNome(String nome){
+        this.nome= nome;
+    }
+    public void setIdade ( int idade){
+        this.idade=idade;
+    }
+    public void setMatricula( int matricula){
+        this.matricula= matricula;
+    }
+    public void setAnoIngresso( int anoIngresso){
+        this.anoIngresso= anoIngresso;
+    }
+    
+}
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+
