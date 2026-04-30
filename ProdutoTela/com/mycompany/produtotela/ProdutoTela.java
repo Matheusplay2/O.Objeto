33bd1082c20a6316b055719fdab8e4a786235748
Matheus Henrique <mhenrique182005@gmail.com>
2026-04-29 21:08:23 -0300

    Add files via upload

---
+++ ProdutoTela/com/mycompany/produtotela/ProdutoTela.java
@@ -0,0 +1,14 @@
+
+package com.mycompany.produtotela;
+
+import com.mycompany.produtotela.gui.frCadProdutos;
+
+
+public class ProdutoTela {
+
+    public static void main(String[] args) {
+        frCadProdutos tela =  new frCadProdutos();
+        
+        tela.setVisible(true);
+    }
+}
