8ed5637cb9e393c059c81b208ad47f7d5da47895
Matheus Henrique <mhenrique182005@gmail.com>
2026-06-03 16:05:10 -0300

    Exception

---
+++ Exception/src/main/java/com/mycompany/exception/Exception.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.exception;
+
+/**
+ *
+ * @author matheus henrique
+ */
+public class Exception {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
